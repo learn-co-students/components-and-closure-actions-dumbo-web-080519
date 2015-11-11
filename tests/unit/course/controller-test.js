9e8928042d02978ac496034662569afdd81881a3
Steven Nunez <steven.nunez@gmail.com>
2015-11-10 22:59:21 -0500

    Fix Controller Test

--- tests/unit/course/controller-test.js
+++ tests/unit/course/controller-test.js
@@ -4,6 +4,7 @@ import {
   describeModule,
   it
 } from 'ember-mocha';
+import Ember from 'ember';
 
 describeModule(
   'controller:courses.course',
@@ -13,9 +14,9 @@ describeModule(
   function() {
     it("toggles a models featured flag", function() {
       var controller = this.subject();
-      controller.set('featured', false);
-      controller.send("toggleFeatured");
-      expect(controller.get('featured')).to.eq(true);
+      controller.set('model', Ember.Object.create({ featured: false }));
+      controller.send("handleFeatured");
+      expect(controller.get('model.featured')).to.eq(true);
     });
   }
 );
